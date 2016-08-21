fba459217cfe2615b662d495cc77dbe45a15a9d4
Homa Wong <homawong@gmail.com>
2016-08-21 00:00:30 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
-declare interface _temp { [index: string]: _temp }
-declare var _temp: any;
-export = _temp;
+declare class _temp {
+    constructor(...args: any[]);
+}
+declare interface _temp2 extends _temp {}
+declare const _temp2: _temp | any;
+
+export = _temp2;
